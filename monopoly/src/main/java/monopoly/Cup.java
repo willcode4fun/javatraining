c06c62357193b0875a81c0c5a71c658371c3751f
Thomas Decoster <thomas.decoster@valtech.com>
2022-03-28 14:25:56 +0200

    Remove solutions from main

--- monopoly/src/main/java/monopoly/Cup.java
+++ monopoly/src/main/java/monopoly/Cup.java
@@ -3,24 +3,4 @@ package monopoly;
 public class Cup {
     // Implement a cup here
 
-    private int faceValue;
-
-    private final Die die1;
-
-    private final Die die2 ;
-
-    public Cup(Die die1, Die die2) {
-        this.die1 = die1;
-        this.die2 = die2;
-    }
-
-    public void roll() {
-        die1.roll();
-        die2.roll();
-        faceValue = die1.getValue()+ die2.getValue();
-    }
-
-    public int getValue() {
-        return faceValue;
-    }
 }
--- monopoly/src/test/java/monopoly/CupTest.java
+++ monopoly/src/test/java/monopoly/CupTest.java
@@ -1,58 +1,7 @@
 package monopoly;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-@RunWith(MockitoJUnitRunner.class)
 public class CupTest {
     // Test your Cup here
 
-    @Spy
-    private Die first;
-    @Spy
-    private Die second;
 
-
-    private Cup cup;
-
-    @Before
-    public void init(){
-
-        /*
-
-        // how to do it without using annotations :
-
-        MockitoAnnotations.openMocks(this);
-        Mockito.mock(Die.class);
-        Mockito.spy(Die.class);
-
-         */
-        cup = new Cup(first, second);
-        when(first.getValue()).thenReturn(1);
-        when(first.getValue()).thenReturn(2);
-
-        when(second.getValue()).thenReturn(1);
-        when(second.getValue()).thenReturn(2);
-    }
-
-
-    @Test
-    public void should_have_value_between_2_and_12() {
-        // Initailisations specific to the test
-        cup.roll(); // Actions on Actors
-        //assertTrue(cup.getValue() >= 2); // Assertions
-        //assertTrue(cup.getValue() <= 12);
-        verify(first).roll();
-        verify(first).getValue();
-
-        verify(second).roll();
-        verify(second).getValue();
-
-    }
 }
